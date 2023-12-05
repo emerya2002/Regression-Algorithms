965c37a4474b8e7680a666fee2a684eb70045c63
Kenechi Okoye <okoyekk@gmail.com>
2023-12-05 17:08:20 -0500

    Added data files and verified their correcteness

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "Util.h"
 // #include <iostream>
 
+#define NUM_DAYS 100 // Number of rows in each csv within data
+
 void test_headers() {
     // Verifies that all header files are linked properly
     // should all print hello with filename.
@@ -13,24 +15,39 @@ void test_headers() {
     util::test();
 }
 
+void verify(std::string folder, std::vector<std::string> filenames) {
+    std::cout << "--" <<  folder << "--" << std::endl;
+    for (auto f : filenames)
+    {
+        std::string file_name = "data/" + folder + "/" + f + ".csv";
+        std::pair<std::vector<std::string>, std::vector<float> > result = parser::parse_csv(file_name);
+        std::cout << f << ": ";
+        if (result.first.size() == NUM_DAYS && result.second.size() == NUM_DAYS)
+        {
+            std::cout << "Data ok - " << NUM_DAYS << " rows" << std::endl;
+        }
+        else
+        {
+            std::cerr << "Data bad - rows must equal " << NUM_DAYS << " rows" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+// Verifies all stocks and variables are complete (exactly NUM_DAYS days of data)
+void verify_data()
+{
+    std::vector<std::string> stocks = {"apple", "boeing", "intel", "nike", "walmart"};
+    verify("stocks", stocks);
+    std::vector<std::string> variables = {"inflation", "interest", "unemployment"};
+    verify("variables", variables);
+}
+
 int main() {
-    test_headers();
+    // test_headers();
+    // verify_data();
 
     // TODO: Add command line arguments to pick a certain stock/variable
-
-    // Testing out parser
-    std::string data_file1 = "data/stocks/sample.csv";
-    std::string data_file2 = "data/variables/sample.csv";
-
-    std::pair<std::vector<std::string>, std::vector<float> > result1 = parser::parse_csv(data_file1);
-    std::pair<std::vector<std::string>, std::vector<float> > result2 = parser::parse_csv(data_file2);
-
-    std::cout << "RESULT1" << std::endl;
-    util::print_vector(result1.first);
-    util::print_vector(result1.second);
-    std::cout << "RESULT2" << std::endl;
-    util::print_vector(result2.first);
-    util::print_vector(result2.second);
 
     return 0;
 }
